df7c344179de43bc00a60fef6c41d7090f04fdc9
freakcoderz <42624259+freakcoderz@users.noreply.github.com>
2020-05-20 06:16:04 -0400

    Initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define CMV Element-qt message queue name
+#define BITCOINURI_QUEUE_NAME "CMV ElementURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
